ff1191148de6554884ab5c95f8d3690f6745ecfc
Igor Zhukov <ihorzhukov@gmail.com>
2023-04-23 12:09:13 +0200

    add recursion exercises

---
+++ Recursion.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+// What will be the output?
+// 90
+
+//func foo(_ function: (Int) -> Int) -> Int {
+//    return function(function(10))
+//}
+//
+//func bar<T: BinaryInteger>(_ number: T) -> T {
+//    return number * 3
+//}
+//
+//print(foo(bar))
